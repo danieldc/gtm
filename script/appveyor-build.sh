59ce78502a20216f94d8f23e47c33406b43a6b39
Michael Schenk <mschenk@theschenks.name>
2017-05-28 02:48:37 -0500

    try again

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -11,7 +11,6 @@ git checkout master
 git submodule update --init
 
 make build-libgit2
-$env:PKG_CONFIG_PATH="C:\gopath\src\github.com\libgit2\git2go\vendor\libgit2\install\lib"
 make install-static
 
 cd /c/gopath/src/github.com/git-time-metric/gtm
