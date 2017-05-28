ec8d5ce3cca33262999188939780372bd8722e8e
Michael Schenk <mschenk@theschenks.name>
2017-05-28 11:50:00 -0500

    Try again

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -5,7 +5,9 @@ export PATH=/c/msys64/mingw64/bin:/c/msys64/usr/bin:/c/Go/bin:/c/gopath/go/bin:$
 export GOROOT=/c/Go/
 export GOPATH=/c/gopath
 
+# remove zlib
 rm C:/msys64/mingw64/lib/libz.dll.a
+rm C:/msys64/mingw64/lib/libz.a
 
 PROJPATH="$GOPATH/src/github.com/libgit2/git2go"
 git clone https://github.com/libgit2/git2go.git $PROJPATH
