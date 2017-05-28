bb188a7b3f77664ea023cc53584eccdfb727cadc
Michael Schenk <mschenk@theschenks.name>
2017-05-28 13:33:54 -0500

    Try again

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -13,9 +13,9 @@ cd vendor/libgit2 && mkdir build
 cd build
 
 sed -i -- 's/ZLIB_FOUND/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
-sed -i -- 's/USE_SSH/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
-# sed -i -- 's/OPENSSL_FOUND/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
-sed -i -- 's/USE_SSH.*"Link with libssh to enable SSH support".*ON/USE_SSH "Link with libssh to enable SSH support" OFF/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
+# sed -i -- 's/USE_SSH/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
+sed -i -- 's/OPENSSL_FOUND/FALSE/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
+sed -i -- 's/USE_SSH.*"Link with libssh to enable SSH support".*ON/USE_SSH  "Link with libssh to enable SSH support"  OFF/g' $PROJPATH/vendor/libgit2/CMakeLists.txt
 
 cat $PROJPATH/vendor/libgit2/CMakeLists.txt
 
@@ -41,10 +41,10 @@ go get -d ./...
 go test --tags static ./...
 
 if [[ "${APPVEYOR_REPO_TAG}" = true ]]; then
-    go build -v -ldflags "-X main.Version=${APPVEYOR_REPO_TAG_NAME}"
+    go build --tags static -v -ldflags "-X main.Version=${APPVEYOR_REPO_TAG_NAME}"
     tar -zcf gtm.${APPVEYOR_REPO_TAG_NAME}.windows.tar.gz gtm.exe
 else
     timestamp=$(date +%s)
-    go build -v -ldflags "-X main.Version=developer-build-$timestamp"
+    go build --tags static -v -ldflags "-X main.Version=developer-build-$timestamp"
     tar -zcf "gtm.developer-build-$timestamp.windows.tar.gz" gtm.exe
 fi
