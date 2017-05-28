8f6f596fa9a42718a14641a08ba75bc3d0b007e1
Michael Schenk <mschenk@theschenks.name>
2017-05-28 12:55:22 -0500

    Try again

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -24,6 +24,7 @@ cmake -DTHREADSAFE=ON \
       -DCMAKE_C_FLAGS=-fPIC \
       -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
       -DCMAKE_INSTALL_PREFIX=../install \
+      -DWINHTTP=OFF \
       -G "MSYS Makefiles" \
       .. &&
 cmake --build . --target install
