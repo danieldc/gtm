2046f86c8a0ca606c3bde4b80cdcbe0b9e99a3a9
Michael Schenk <mschenk@theschenks.name>
2017-05-28 15:02:46 -0500

    Try again

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -3,7 +3,7 @@ set -ex
 
 export PATH=/c/msys64/mingw64/bin:/c/msys64/usr/bin:/c/Go/bin:/c/gopath/go/bin:$PATH
 
-GIT2GO_PATH=$GOPATH/src/github.com/libgit2/git2go
+GIT2GO_PATH=/c/gopath/src/github.com/libgit2/git2go
 cd $GIT2GO_PATH/vendor/libgit2 && mkdir build && cd build
 LIBGIT2_BUILD=$GIT2GO_PATH/vendor/libgit2/build
 FLAGS="-lws2_32"
