0e102489180102026db3fd6760584e4d17470910
Michael Schenk <mschenk@theschenks.name>
2017-05-28 11:53:44 -0500

    Try again

--- script/appveyor-build.sh
+++ script/appveyor-build.sh
@@ -6,8 +6,8 @@ export GOROOT=/c/Go/
 export GOPATH=/c/gopath
 
 # remove zlib
-rm C:/msys64/mingw64/lib/libz.dll.a
-rm C:/msys64/mingw64/lib/libz.a
+mingw-get uninstall zlib
+mingw-get uninstall msys-zlib-dev
 
 PROJPATH="$GOPATH/src/github.com/libgit2/git2go"
 git clone https://github.com/libgit2/git2go.git $PROJPATH
